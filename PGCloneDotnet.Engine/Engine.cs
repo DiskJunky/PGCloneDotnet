a7a88b8ce381a5d2b1606b9081df3cbe0e331972
DiskJunky <diskjunky@hotmail.com>
2025-12-23 22:16:15 +0000

    Added pseudocode for a very basic game loop to start with

---
+++ PGCloneDotnet.Engine/Engine.cs
@@ -0,0 +1,53 @@
+namespace PGCloneDotNet.Engine;
+
+/// <summary>
+/// This manages the main game elements, takes the game configuration, and progresses
+/// characters.
+/// </summary>
+public class Engine
+{
+    /// <summary>
+    /// The main program loop that iterates the game along one tick in game time.
+    /// </summary>
+    public void DoTick()
+    {
+        // roll character
+        
+        // map story progress {
+            // generate story {
+                // Prologue
+                // Act 1
+                // Act 2
+                // Act 3
+                // Epilogue
+            // }
+            
+            // start player at story point 0;
+            
+            // determine story point completion criteria?
+        // }
+        
+        
+        // start in origin
+        // go to place (determine distance to travel)
+        
+        // for each unit travelled [one unit per tick] {
+            // random chance of encounter (50%)
+            // if encounter {
+                // do {
+                    // hit/take damage
+                // } until (dead || monster killed)
+                
+                // if (dead) {
+                    // return to origin, lose experience
+                // }
+                
+                // gain experience/gold
+            // }
+            
+            // rest(?)
+        // } until reached destination
+        
+        // print score
+    }
+}
